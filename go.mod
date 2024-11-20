0a277a98bcfe2a41dbb1d5e452c8de833970d43e
Elina Kushmar <elina.kushmar@gmail.com>
2024-11-20 17:16:59 +0300

    add models

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/marelinaa/visa-api
+
+go 1.22.1
+
+require (
+	github.com/lib/pq v1.10.9
+	github.com/pressly/goose v2.7.0+incompatible
+)
+
+require github.com/pkg/errors v0.9.1 // indirect
---
+++ services/visa/internal/domain/models.go
@@ -0,0 +1,47 @@
+package domain
+
+type Application struct {
+	ID             int64    `json:"id"`
+	SubmissionDate string   `json:"sub_date"`
+	Status         string   `json:"status"`
+	Documents      []string `json:"docs"`
+	ApplicantID    int64    `json:"applicant_id"`
+	PaymentID      int64    `json:"payment_id"`
+	OperatorID     int64    `json:"operator_id"`
+}
+
+type User struct {
+	ID           int64  `json:"id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	PhoneNumber  string `json:"phone_num" validate:"required,e164"`
+	Email        string `json:"email"`
+	PasswordHash string `json:"password_hash"`
+	Role         string `json:"role"`
+}
+
+type Applicant struct {
+	ID           int64  `json:"id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	PhoneNumber  string `json:"phone_num" validate:"required,e164"`
+	Email        string `json:"email"`
+	PasswordHash string `json:"password_hash"`
+}
+
+type Payment struct {
+	ID             int64   `json:"id"`
+	Sum            float64 `json:"sum"`
+	PaymentDate    string  `json:"payment_date"`
+	Status         string  `json:"status"`
+	ApplicantionID int64   `json:"applicantion_id"`
+}
+
+type Operator struct {
+	ID           int64  `json:"id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	PhoneNumber  string `json:"phone_num" validate:"required,e164"`
+	Email        string `json:"email"`
+	PasswordHash string `json:"password_hash"`
+}
